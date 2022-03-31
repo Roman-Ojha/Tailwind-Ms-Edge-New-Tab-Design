39a2f664caa121f67f0c218a0917f20cbb781931
Roman Ojha <razzroman98@gmail.com>
2022-04-01 00:39:31 +0545

    completing design of upper nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
         "body-background":
           "url('https://images.unsplash.com/photo-1532274402911-5a369e4c4bb5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')",
       },
+      fontFamily: {
+        body: ["Nunito", "sans-serif"],
+      },
     },
   },
   plugins: [],
